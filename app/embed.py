509097f36dd1daa68ab9eb36a98f542df8081698
Ayush Kumar <ayush@maverick>
2025-06-18 12:50:10 +0530

    important files

---
+++ app/embed.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import json
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Load metadata
+def load_embeddings():
+    path = os.path.join(BASE_DIR, "data/embeddings.npy")
+    return np.load(path)
+
+# Load precomputed embeddings
+def load_metadata():
+    path = os.path.join(BASE_DIR, "data/metadata.json")
+    with open(path) as f:
+        return json.load(f)
